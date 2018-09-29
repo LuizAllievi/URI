7dea40d032c8eb8a3146fcdde3dd8689a583a1ec
Luiz Gustavo <luizgu2000@gmail.com>
2018-09-29 16:14:07 -0300

    DDD

---
+++ 1050.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+A = int(input())
+
+if (A == 11):
+    print("Sao Paulo")
+elif (A == 19):
+    print("Campinas")
+elif (A == 21):
+    print("Rio de Janeiro")
+elif (A == 27):
+    print("Vitoria")
+elif (A == 31):
+    print("Belo Horizonte")
+elif (A == 32):
+    print("Juiz de Fora")
+elif (A == 61):
+    print("Brasilia")
+elif (A == 71):
+    print("Salvador")
+else:
+    print("DDD nao cadastrado")
