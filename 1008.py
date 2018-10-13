cd9d585d8ed76786bd8a0700d36a8a9d89e78dda
Luiz Gustavo <luizgu2000@gmail.com>
2018-10-13 13:52:27 -0300

    Salário

---
+++ 1008.py
@@ -0,0 +1,8 @@
+numero = int(input())
+hours = int(input())
+hour_price = float(input())
+
+salary = hour_price * hours
+
+print("NUMBER = {}".format(numero))
+print("SALARY = U$ {:.2f}".format(salary))
